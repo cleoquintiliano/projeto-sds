91e2c6d8d439eba3f0bb3e78c235e18d5eee4b4c
cleoquintiliano <cleoquintiliano@gmail.com>
2021-11-07 01:06:20 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -5,8 +5,8 @@ import java.io.Serializable;
 import com.devsuperior.dsvendas.entities.Seller;
 
 public class SellerDTO implements Serializable{
-
 	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private String name;
 	
